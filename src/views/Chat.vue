c58ed81fd00f5b25a9b24bcdb96a1709a3f08cb2
yemiwebby <yemiwebby@gmail.com>
2020-08-19 11:08:26 +0100

    initial commit

---
+++ src/views/Chat.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="page-wrapper">
+      <p>This is the chat view</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
